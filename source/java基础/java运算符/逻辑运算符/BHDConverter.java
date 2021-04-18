7757de4a50077a6a11c071f34d7da429bf014a5d
CrazyFeirouz <57065210+CrazyFeirouz@users.noreply.github.com>
2021-04-18 20:51:33 +0800

    新建进制转换模拟

---
+++ source/java基础/java运算符/逻辑运算符/BHDConverter.java
@@ -0,0 +1,14 @@
+package java基础.java运算符.逻辑运算符;
+
+/**
+ * -*- coding: utf-8 -*-
+ * @Author Feirouz
+ * @Date 2021/4/18 20:49
+ * @Des 模拟进制转换, 使用 & 和 位移运算 来实现
+ */
+
+public class BHDConverter {
+    public static void main(String[] args) {
+
+    }
+}
