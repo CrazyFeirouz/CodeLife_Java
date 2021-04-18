3221bcf5dfa47afac692e2674197b15e488600bf
CrazyFeirouz <57065210+CrazyFeirouz@users.noreply.github.com>
2021-04-18 21:58:28 +0800

    十进制转十六进制模拟

--- source/basic/operator/logic/BHDConverter.java
+++ source/basic/operator/logic/BHDConverter.java
@@ -9,10 +9,32 @@ package basic.operator.logic;
 
 public class BHDConverter {
     public static void main(String[] args) {
-        // 方式一：自动实现
-        String str1 = Integer.toBinaryString(60);
-        String str2 = Integer.toHexString(60);
+        binToHex(60);
+    }
 
-        System.out.println("二进制：" + str1 + "；十六进制：" + str2);
+    /**
+     * 十进制 转 十六进制
+     * @param num 需要转换的数
+     */
+    public static void binToHex(int num){
+        //实现0-255的转换，255 - 1111 1111
+
+        // 方式一：自动实现
+//        String str2 = Integer.toHexString(num);
+//        System.out.println(str2);
+
+        // 方式二：手动实现
+
+        // 转换第一位Hex
+        int transNum1 = num & 15;       //15 - 1111，取出二进制 后4位数
+        String hex1 = transNum1 > 9 ? (char)(transNum1 - 10 + 'A') + "" : transNum1 + "";   //十六进制数 1-9， A-F
+
+        // 转换第二位Hex
+        num = num >>> 4;        //右移4位，去除刚刚已取出的4位
+        int transNum2 = num & 15;       //同理
+        String hex2 = transNum2 > 9 ? (char)(transNum2 - 10 + 'A') + "" : transNum2 + "";
+
+        System.out.println(hex2 + "" + hex1);
+
     }
 }
