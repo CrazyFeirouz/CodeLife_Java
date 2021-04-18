cd1f1851dc7351c788b6616591f0e9b3e887fb17
CrazyFeirouz <57065210+CrazyFeirouz@users.noreply.github.com>
2021-04-18 22:56:52 +0800

    查找质数新建

---
+++ source/basic/structure/nestedloop/PrimeNumber.java
@@ -0,0 +1,14 @@
+package basic.structure.nestedloop;
+
+/**
+ * -*- coding: utf-8 -*-
+ * @Author Feirouz
+ * @Date 2021/4/18 22:55
+ * @Des 查找质数 - 利用不同的方法
+ */
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+
+    }
+}
