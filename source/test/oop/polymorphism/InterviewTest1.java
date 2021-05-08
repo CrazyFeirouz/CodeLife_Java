f45926949fb1062b87a5dbeb11ab7cbc75cd11cf
CrazyFeirouz <feirouz@foxmail.com>
2021-05-08 20:18:09 +0800

    多态性学习4

---
+++ source/test/oop/polymorphism/InterviewTest1.java
@@ -0,0 +1,29 @@
+package test.oop.polymorphism;
+
+// 前提类
+class Base2 {
+    public void add(int a, int... arr) {    // int[] arr 可看作 int... arr ，这里可看作重写
+        System.out.println("base2");
+    }
+}
+class Sub2 extends Base2 {
+    public void add(int a, int[] arr) {     // int[] arr 可看作 int... arr ，这里可看作重写
+        System.out.println("sub2_1");
+    }
+    public void add(int a, int b, int c) {
+        System.out.println("sub2_2");
+    }
+}
+
+public class InterviewTest1 {
+    public static void main(String[] args) {
+        Base2 base = new Sub2();
+        //这里由于多态性，那么必定看左边父类存在的方法先，运行时再执行子类重写父类的方法。
+        base.add(1, 2, 3);  //sub2_1
+
+
+        Sub2 s = (Sub2)base;
+        //这里由于直接调用子类方法，那么确定的方法优先调用
+        s.add(1,2,3);   //sub2_2
+    }
+}
