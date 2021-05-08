6e6e4316c5ca375c94c872fa8ce760eabe022e58
CrazyFeirouz <feirouz@foxmail.com>
2021-05-08 15:06:16 +0800

    多态性学习

--- source/test/oop/polymorphism/test1/Person.java
+++
@@ -1,9 +0,0 @@
-package test.oop.polymorphism.test1;
-
-public class Person {
-    public int age;
-
-    public void introduction(){
-        System.out.println("我是普通人");
-    }
-}
--- source/test/oop/polymorphism/test1/PersonTest.java
+++ source/test/oop/polymorphism/test1/PersonTest.java
@@ -1,6 +1,6 @@
 package test.oop.polymorphism.test1;
 
-public class Test {
+public class PersonTest {
     public static void main(String[] args) {
         Person p1 = new Person();
         p1.introduction();
@@ -21,5 +21,36 @@ public class Test {
 
     public static void pIntroduction(Person p){
         p.introduction();
+    }
+}
+
+class Person {
+    public int age;
+
+    public void introduction(){
+        System.out.println("我是普通人");
+    }
+}
+
+class Man extends Person{
+    public int power = 100;
+
+    public void manIntroduction(){
+        System.out.println("我的力量值是" + power);
+    }
+
+    public void introduction(){
+        System.out.println("我是男人");
+    }
+}
+
+class Woman extends Person{
+    public int charm = 100;
+
+    public void womanIntroduction(){
+        System.out.println("我的魅力值是" + charm);
+    }
+    public void introduction(){
+        System.out.println("我是女人");
     }
 }
