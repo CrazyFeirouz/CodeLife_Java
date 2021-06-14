92e8fd620669e00724e46381771204cb683e3bf8
CrazyFeirouz <feirouz@foxmail.com>
2021-06-14 16:45:39 +0800

    String的实例化：不同方式导致的区别

--- source/test/api/strings/StringTest.java
+++ source/test/api/strings/StringTest.java
@@ -31,4 +31,38 @@ public class StringTest {
         System.out.println(s1 == s4);   //false
         System.out.println(s3 == s4);   //false
     }
+
+    /**
+     * Java intern() 方法
+     *      intern() 方法返回字符串对象的规范化表示形式。
+     *          但是这个方法会首先检查字符串池中是否有”ab”这个字符串，如果存在则返回这个字符串的引用，
+     *          否则就将这个字符串添加到字符串池中，然会返回这个字符串的引用。
+     *
+     * 结论
+     *  1. 常量与常量的拼接结果在常量池。且常量池中不会存在相同内容的常量
+     *  2. 只要其中一个是变量，结果就在堆中
+     *  3. 如果拼接的结果调用intern()方法，返回值就在常量池中
+     */
+    @Test
+    public void test2() {
+        String s1 = "javaEE";
+        String s2 = "hadoop";
+
+        String s3 = "javaEEhadoop";
+        String s4 = "javaEE" + "hadoop";
+        String s5 = s1 + "hadoop";
+        String s6 = "javaEE" + s2;
+        String s7 = s1 + s2;
+
+        System.out.println(s3 == s4);   //true
+        System.out.println(s3 == s5);   //false
+        System.out.println(s3 == s6);   //false
+        System.out.println(s3 == s7);   //false
+        System.out.println(s5 == s6);   //false
+        System.out.println(s5 == s7);   //false
+        System.out.println(s6 == s7);   //false
+
+        String s8 = s5.intern();    //返回值 = 常量池中已经存在的“javaEEhadoop”
+        System.out.println(s3 == s8);   //true
+    }
 }
