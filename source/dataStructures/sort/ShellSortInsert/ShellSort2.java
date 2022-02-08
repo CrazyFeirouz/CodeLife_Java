0fa3e4dc18af86bff61f80734b7fda54d0ec0c9b
CrazyFeirouz <feirouz@foxmail.com>
2022-02-08 12:42:50 +0800

    排序-希尔排序

--- source/dataStructures/sort/ShellSortInsert/ShellSort2.java
+++ source/dataStructures/sort/ShellSortInsert/ShellSort2.java
@@ -1,4 +1,4 @@
-package dataStructures.sort;
+package dataStructures.sort.ShellSortInsert;
 
 import org.junit.Test;
 
--- source/dataStructures/sort/ShellSortSwap/ShellSort1.java
+++ source/dataStructures/sort/ShellSortSwap/ShellSort1.java
@@ -1,4 +1,4 @@
-package dataStructures.sort;
+package dataStructures.sort.ShellSortSwap;
 
 import org.junit.Test;
 
