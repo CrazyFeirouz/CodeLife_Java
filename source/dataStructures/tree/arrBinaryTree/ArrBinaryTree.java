5fd872db22b82026fdf78fab26fafa072740b972
CrazyFeirouz <feirouz@foxmail.com>
2022-02-22 00:27:23 +0800

    顺序存储二叉树 前序、中序、后续遍历

---
+++ source/dataStructures/tree/arrBinaryTree/ArrBinaryTree.java
@@ -0,0 +1,77 @@
+package dataStructures.tree.arrBinaryTree;
+
+public class ArrBinaryTree {
+    private int[] arr;  // 存储数据节点的数组
+
+    public ArrBinaryTree(int[] arr) {
+        this.arr = arr;
+    }
+
+    // 重载该方法
+    public void preorder() {
+        this.preorder(0);
+    }
+
+    // 编写一个方法, 完成顺序存储二叉树的前序遍历
+    public void preorder(int index) {
+        // 如果数组为空 或者 arr.length = 0;
+        if (arr == null || arr.length == 0) {
+            System.out.println("数组为空, 不能按照二叉树的前序遍历");
+        }
+        // 输出当前这个元素
+        System.out.println(arr[index]);
+        // 向左递归遍历
+        if ((index * 2 + 1) < arr.length) {
+            preorder(2 * index + 1);
+        }
+        // 向右递归遍历
+        if ((index * 2 + 2) < arr.length) {
+            preorder(2 * index + 2);
+        }
+    }
+
+    public void inorder() {
+        this.inorder(0);
+    }
+
+    // 编写一个方法, 完成顺序存储二叉树的中序遍历
+    public void inorder(int index) {
+        // 如果数组为空 或者 arr.length = 0;
+        if (arr == null || arr.length == 0) {
+            System.out.println("数组为空, 不能按照二叉树的前序遍历");
+        }
+        // 向左递归遍历
+        if ((index * 2 + 1) < arr.length) {
+            preorder(2 * index + 1);
+        }
+        // 输出当前这个元素
+        System.out.println(arr[index]);
+        // 向右递归遍历
+        if ((index * 2 + 2) < arr.length) {
+            preorder(2 * index + 2);
+        }
+    }
+
+    public void postorder() {
+        this.inorder(0);
+    }
+
+    // 编写一个方法, 完成顺序存储二叉树的后序遍历
+    public void postorder(int index) {
+        // 如果数组为空 或者 arr.length = 0;
+        if (arr == null || arr.length == 0) {
+            System.out.println("数组为空, 不能按照二叉树的前序遍历");
+        }
+        // 向左递归遍历
+        if ((index * 2 + 1) < arr.length) {
+            preorder(2 * index + 1);
+        }
+        // 向右递归遍历
+        if ((index * 2 + 2) < arr.length) {
+            preorder(2 * index + 2);
+        }
+        // 输出当前这个元素
+        System.out.println(arr[index]);
+    }
+}
+
---
+++ source/dataStructures/tree/arrBinaryTree/ArrBinaryTreeDemo.java
@@ -0,0 +1,18 @@
+package dataStructures.tree.arrBinaryTree;
+
+public class ArrBinaryTreeDemo {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+
+        ArrBinaryTree arrBinaryTree = new ArrBinaryTree(arr);
+
+        System.out.println("======前序遍历=======");
+        arrBinaryTree.preorder();
+
+        System.out.println("======中序遍历=======");
+        arrBinaryTree.preorder();
+
+        System.out.println("======后续遍历=======");
+        arrBinaryTree.preorder();
+    }
+}
