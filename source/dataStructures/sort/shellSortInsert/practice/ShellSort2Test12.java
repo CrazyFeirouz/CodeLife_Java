7172078ca5df3c30e7123d7849c3bb83417a66fe
CrazyFeirouz <feirouz@foxmail.com>
2022-05-09 01:13:50 +0800

    测验

---
+++ source/dataStructures/sort/shellSortInsert/practice/ShellSort2Test12.java
@@ -0,0 +1,31 @@
+package dataStructures.sort.shellSortInsert.practice;
+
+import java.util.Arrays;
+
+/**
+ * @description: 希尔排序 (插入法)
+ * @author: Feirouz
+ * @date: 2022-05-09 1:00
+ */
+public class ShellSort2Test12 {
+    public static void main(String[] args) {
+        int[] arr = {6,8,1,2,3,-4,8,-9,5,10};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void sort(int[] arr) {
+        int temp = -1;
+        for (int gap = arr.length/2; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                int curVal = arr[i];
+                int j = i - gap;
+                while (j >= 0 && arr[j] > curVal) {
+                    arr[j + gap] = arr[j];
+                    j -= gap;
+                }
+                arr[j + gap] = curVal;
+            }
+        }
+    }
+}
