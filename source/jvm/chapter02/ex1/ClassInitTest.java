7316376d099da4f5723f97f87135a6f1030161ea
CrazyFeirouz <feirouz@foxmail.com>
2022-09-06 15:53:46 +0800

    类的加载过程

--- source/jvm/chapter02/ex1/ClassInitTest.java
+++ source/jvm/chapter02/ex1/ClassInitTest.java
@@ -22,3 +22,43 @@ public class ClassInitTest {
         System.out.println(ClassInitTest.number);   // 10
     }
 }
+
+
+/**
+ * .class文件
+ * public class ClassInitTest {
+ *     private static int num = 1;
+ *     private static int number;
+ *
+ *     public ClassInitTest() {
+ *     }
+ *
+ *     public static void main(String[] args) {
+ *         System.out.println(num);
+ *         System.out.println(number);
+ *     }
+ *
+ *     static {
+ *         num = 2;
+ *         number = 20;
+ *         System.out.println(num);
+ *         number = 10;
+ *     }
+ * }
+ */
+
+/**
+ * <clinit>方法
+ *  0 iconst_1
+ *  1 putstatic #3 <jvm/chapter02/ex1/ClassInitTest.num>
+ *  4 iconst_2
+ *  5 putstatic #3 <jvm/chapter02/ex1/ClassInitTest.num>
+ *  8 bipush 20
+ * 10 putstatic #5 <jvm/chapter02/ex1/ClassInitTest.number>
+ * 13 getstatic #2 <java/lang/System.out>
+ * 16 getstatic #3 <jvm/chapter02/ex1/ClassInitTest.num>
+ * 19 invokevirtual #4 <java/io/PrintStream.println>
+ * 22 bipush 10
+ * 24 putstatic #5 <jvm/chapter02/ex1/ClassInitTest.number>
+ * 27 return
+ */
