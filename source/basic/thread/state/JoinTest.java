d138491799cb6a5ff0cda8a91bf8a76910edcce6
CrazyFeirouz <feirouz@foxmail.com>
2023-06-06 12:59:47 +0800

    线程状态

--- source/basic/thread/state/JoinTest.java
+++ source/basic/thread/state/JoinTest.java
@@ -5,5 +5,26 @@ package basic.thread.state;
  * @author: Feirouz
  * @date: 2023-06-05 21:06
  */
-public class JoinTest {
+public class JoinTest implements Runnable{
+    @Override
+    public void run() {
+        for (int i = 0; i < 200; i++) {
+            System.out.println("线程vip来了... ==" + i + "== 闪亮登场");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        // 启动我们的线程
+        JoinTest joinTest = new JoinTest();
+        Thread thread = new Thread(joinTest);
+        thread.start();
+
+        // 主线程
+        for (int i = 0; i < 300; i++) {
+            if (i == 10){
+                thread.join();  // 排队
+            }
+            System.out.println("main线程回归... --" + i + "--");
+        }
+    }
 }
---
+++ source/basic/thread/state/StateTest.java
@@ -0,0 +1,38 @@
+package basic.thread.state;
+
+/**
+ * @description:
+ * @author: Feirouz
+ * @date: 2023-06-06 12:44
+ */
+
+// 观察测试线程的状态
+public class StateTest {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(()->{
+            for (int i = 0; i < 5; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("/////////");
+        });
+
+        // 观察状态
+        Thread.State state = thread.getState();
+        System.out.println(state);
+
+        // 观察启动后
+        thread.start(); // 启动线程
+        state = thread.getState();
+        System.out.println(state);  // RUNNABLE
+
+        while (state != Thread.State.TERMINATED){   // 只要线程不终止，就一直输出状态
+            Thread.sleep(100);
+            System.out.println(state);
+            state = thread.getState();  // 更新线程状态
+        }
+    }
+}
